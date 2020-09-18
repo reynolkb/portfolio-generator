cef237c284e122f1c4a1bfb14d264ee16ced3823
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-09-18 14:46:35 -0700

    added function to generate html page

---
+++ app.js
@@ -0,0 +1,20 @@
+const fs = require('fs'); // allows the file to access the fs module's functions through the fs assignment.
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+// console.log(name, github); // logs to the console only name and github
+// console.log(generatePage(name, github)); // logs to the console the return of the function with parameters name and github
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
